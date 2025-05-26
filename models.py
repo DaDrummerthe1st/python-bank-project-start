c70a71d90124812cc5aa12117a57723a8c5be96f
Kymia <Kymia.jm@icloud.com>
2025-05-26 16:12:29 +0200

    normalization for customers

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import declarative_base, relationship
 Base = declarative_base()
 
 class Customer(Base):
-    __tablename__ = "customer"
+    __tablename__ = "customers" # tabell namn som vi skapar 
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     ssn = Column(String, unique=True, nullable=False)
@@ -12,13 +12,13 @@ class Customer(Base):
     accounts= relationship("Account", back_populates="customer")
     
 class Account(Base):
-    __tablename__ = "account"
+    __tablename__ = "accounts" # tabell namn 
     id = Column(Integer, primary_key=True)
     number = Column(String, nullable=False)
-    balance = Column(String, nullable=False)
+    balance = Column(Numeric, nullable=False)
     credit = Column(Numeric, default=0)
     interest = Column(Numeric, default=0)
     currency = Column(String(3), nullable=False)
     
-    customer_id = Column(Integer, ForeignKey("customer.id"))
+    customer_id = Column(Integer, ForeignKey("customers.id"))
     customer = relationship("Customer", back_populates="accounts")
